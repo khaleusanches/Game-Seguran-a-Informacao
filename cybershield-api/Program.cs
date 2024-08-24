4e929854ad3bef3b43fed9862f7561d1d181bbff
khaleusanches <khaleusanches@gmail.com>
2024-08-24 08:54:00 -0300

    railway

--- cybershield-api/Program.cs
+++ cybershield-api/Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8081";
+builder.WebHost.UseUrls($"http://*:{port}");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
